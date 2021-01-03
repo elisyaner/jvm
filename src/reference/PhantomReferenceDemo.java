5eba614082fd04015587eeaee07c70fd288f20b4
yan.zhang <13721133018@163.com>
2021-01-03 12:39:46 +0800

    虚引用

---
+++ src/reference/PhantomReferenceDemo.java
@@ -0,0 +1,47 @@
+/*
+ * All rights Reserved, Designed By baowei
+ *
+ * 注意：本内容仅限于内部传阅，禁止外泄以及用于其他的商业目的
+ */
+package reference;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+
+/**
+ * @author yan.zhang
+ * @date 2021/1/3 12:30
+ */
+public class PhantomReferenceDemo {
+    /**
+     * 虚引用
+     * 配合引用队列使用
+     * 虚引用作用主要是跟踪对象被垃圾回收的状态，仅仅提供一种确保对象被finalize之后，做某些事情的机制
+     * 设置虚引用关联的唯一目的，就是在这个对象被垃圾回收的时候收到一个系统通知或后续添加进一步处理。
+     * java允许使用finalize()方法在垃圾回收器将对象从内存中清除之前做必要的清理动作
+     *
+     * @param args
+     */
+    public static void main(String[] args) throws InterruptedException {
+        Object o1 = new Object();
+        ReferenceQueue<Object> queue = new ReferenceQueue<>();
+        PhantomReference<Object> phantomReference = new PhantomReference<>(o1, queue);
+
+        System.out.println(o1);
+        //对于虚引用，get()只会返回null
+        System.out.println(phantomReference.get());
+        System.out.println(queue.poll());
+
+        System.out.println("==================");
+
+        o1 = null;
+        System.gc();
+        Thread.sleep(500);
+
+        System.out.println(o1);
+        System.out.println(phantomReference.get());
+        System.out.println(queue.poll());
+
+    }
+
+}
